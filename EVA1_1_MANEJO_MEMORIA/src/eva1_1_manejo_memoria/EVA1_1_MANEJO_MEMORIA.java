d1f3c6affe13b26d86c676aee4b54cc0f1714853
YajairaMoran <159494447+YajairaMoran@users.noreply.github.com>
2025-01-30 17:40:25 -0600

    Add files via upload

---
+++ EVA1_1_MANEJO_MEMORIA/src/eva1_1_manejo_memoria/EVA1_1_MANEJO_MEMORIA.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_manejo_memoria;
+
+/**
+ *
+ * @author usuario
+ */
+public class EVA1_1_MANEJO_MEMORIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Inicia main");
+        A();
+        System.out.println("Termina main");
+        
+    }
+    public static void A(){
+        System.out.println("Inicia A");
+        B();
+        System.out.println("Termina A");
+    }
+    public static void B(){
+        System.out.println("Inicia B");
+        System.out.println("Termina B");
+    }
+
+}
