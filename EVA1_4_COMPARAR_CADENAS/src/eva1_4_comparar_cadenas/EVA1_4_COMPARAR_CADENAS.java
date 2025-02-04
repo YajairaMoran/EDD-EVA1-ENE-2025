72becc007ac52a31db50a670f5c37fad160c88b6
YajairaMoran <159494447+YajairaMoran@users.noreply.github.com>
2025-02-04 17:52:33 -0600

    Add files via upload

---
+++ EVA1_4_COMPARAR_CADENAS/src/eva1_4_comparar_cadenas/EVA1_4_COMPARAR_CADENAS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_comparar_cadenas;
+
+/**
+ *
+ * @author usuario
+ */
+public class EVA1_4_COMPARAR_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String cade1= "HOLA";
+        String cade2= "HOLA";
+        
+        if (cade1.equals(cade2))
+        if (cade1 == cade2)
+            System.out.println("Cadenas iguales");
+        else 
+            System.out.println("Cadenas diferentes");
+    }
+    
+}
